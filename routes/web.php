4fc9e82127394d1226c72358f054d4dd2203ae32
Alex Paul <alex_12paul@hotmail.es>
2021-12-10 00:46:28 -0500

    sub

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TermodinamicaController;
+use App\Http\Controllers\SoundController;
+use App\Http\Controllers\EnergyController;
+use App\Http\Controllers\ForceController;
+use App\Http\Controllers\PendularController;
+use App\Http\Controllers\ParabolController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +25,9 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+Route::get('/termodinamica', [TermodinamicaController::class, 'index'])->name('termodinamica');
+Route::get('/fuerza', [ForceController::class, 'index'])->name('fuerza');
+Route::get('/energia', [EnergyController::class, 'index'])->name('energia');
+Route::get('/pendulo', [PendularController::class, 'index'])->name('pendulo');
+Route::get('/parabolico', [ParabolController::class, 'index'])->name('parabolico');
+Route::get('sonido', [SoundController::class, 'index'])->name('sonido');
